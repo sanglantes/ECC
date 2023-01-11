4cad83c1c7272ed5fd4a352a7cf92147bd48818a
sanglantes <paul.holmberg@elev.ga.lbs.se>
2023-01-12 00:41:43 +0100

    Beginning to refactor from getopt to getopt_long

--- src/parser.c
+++ src/parser.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <getopt.h>
 #include <gmp.h>
 #include "status.h"
 
-struct Arguments {
-	mpz_t			a;
-	mpz_t			b;
-	unsigned int	s;
-	unsigned int	r;
-};
-
 int main(int argc, char* argv[]) {
-	struct Arguments argument = { .r = 0 };
+
+	int preset_flag;
+	int form_flag;
+	static struct option longopts[] = {
+		{ NULL, 	required_argument, 	NULL, 		'a' },
+		{ NULL, 	required_argument, 	NULL, 		'b' },
+		{ "size", 	required_argument, 	NULL, 		's' },
+		{ "preset", 	required_argument, 	&preset_flag, 	'p' },
+		{ "form", 	required_argument, 	&form_flag, 	'f' },
+		{ "graph", 	no_argument, 		&graph_flag, 	'g' },
+		{ "help", 	no_argument, 		NULL, 		'h' }
+
+	}
 	int c;
-	// a	CONSTANT A
-	// b	CONSTANT B
-	// s	KEY SIZE
 	while((c = getopt(argc, argv, "a:b:s:r")) != -1) {
 		switch(c)
 		{
@@ -59,17 +61,18 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, 
 "usage: ./ecc <-a integer_constant> <-b integer_constant> <-s key_size> [-p presets] [-f file] [-g] [-h]\n"
 "\n"
-"		Generates elliptic curves for educational and cryptographical purposes. Performs elliptic curve arithemtic.\n"
+"  Generates elliptic curves for educational and cryptographical purposes. Performs elliptic curve arithemtic.\n"
 "\n"
-"		options:\n"
-			"-h, --help			display this message\n"
-			"-a					select the 'a' constant in an EC curve.\n"
-			"-b					select the 'b' constant in an EC curve.\n"
-			"-s, --size			bit size for the key (recommended: 256)\n"
-			"-p, --preset=FORM	specify a different equation form (default: shortw)\n"
-			"						(options: \"shortw\", \"longw\", \"edward\", \"montgomery\", \"hessian\")\n"
-			"-f, --file=FILE	read configuration from a file\n"
-			"-g, --graph		graph a plot of the curve\n"
+"  options:\n"
+"   -a			select the 'a' constant in an EC curve.\n"
+"   -b			select the 'b' constant in an EC curve.\n"
+"   -s, --size		bit size for the key (recommended: 256)\n"
+"   -p, --preset=PRESET choose a preconfigured curve\n"
+"			   (options: \"brainpool256\", \"curve25519\", \"nist256\", \"secp256k1\")\n"
+"   -f, --form=FORM	specify a different equation form (default: shortw)\n"
+"			   (options: \"shortw\", \"longw\", \"edward\", \"montgomery\", \"hessian\")\n"
+"   -g, --graph		graph a plot of the curve\n"
+"   -h, --help		display this message\n"
 
 		);
 	}
