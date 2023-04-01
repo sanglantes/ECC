4217acc36f5048517729f7953577e9191810f3eb
sanglantes <101125878+sanglantes@users.noreply.github.com>
2023-04-01 20:09:07 +0000

    Update curves.c
    OCD space!

--- src/curves.c
+++ src/curves.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <gmp.h>
 #include "curves.h"
+
 // ************************************************************************
 // *									  *
 // *                            NOTES ON curves.c                         *
