440ace0cc8e7b6927ed56fae4b678d436bbae7c2
sanglantes <paul.holmberg@elev.ga.lbs.se>
2023-04-04 12:19:00 +0200

    Safety before merge

--- src/curves.c
+++ src/curves.c
@@ -82,7 +82,7 @@ curve_t create_curve25519() {
 // * 	B = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b		  *
 // *	Base point = ("6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", *
 // *                  "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5") *
-// * Additional notes: Not safe when used with DUAL_EC_DRBG			  *
+// * Additional notes: Not safe when used with DUAL_EC_DRBG			          *
 // *											  *
 // ****************************************************************************************
 curve_t create_p256() {
